b51efa33347b0cd4268b0be9a955c2665cee8f09
David B. Lamkins <david@lamkins.net>
2020-12-25 15:13:59 -0800

    Add preflight check.

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 #! /bin/sh
 
+if [ ! -w /etc ]; then
+	echo $USER can not write /etc
+	exit 1
+fi
 sudo install -v -m 755 -d /etc/xs/rc.d /etc/xs/lib.d
 for f in xs/rc.d/*; do
 	sudo install -v -b -C -m 644 $f /etc/xs/rc.d/
