96f82d830ad110d14cddde308dd96a55e2bd6c22
David B. Lamkins <david@lamkins.net>
2020-12-24 18:03:11 -0800

    Remove debug code.

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-echo sudo install -m 755 -d /etc/xs/rc.d /etc/xs/lib.d
+sudo install -m 755 -d /etc/xs/rc.d /etc/xs/lib.d
 for f in xs/rc.d/*; do
 	sudo install -m 644 $f /etc/xs/rc.d/
 done
