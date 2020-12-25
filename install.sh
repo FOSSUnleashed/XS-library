edf1fefe6d1479c7c42491c56c9dc7ad835f510f
David B. Lamkins <david@lamkins.net>
2020-12-24 18:04:13 -0800

    Install verbosely.

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #! /bin/sh
 
-sudo install -m 755 -d /etc/xs/rc.d /etc/xs/lib.d
+sudo install -v -m 755 -d /etc/xs/rc.d /etc/xs/lib.d
 for f in xs/rc.d/*; do
-	sudo install -m 644 $f /etc/xs/rc.d/
+	sudo install -v -m 644 $f /etc/xs/rc.d/
 done
 for f in xs/lib.d/*; do
-	sudo install -m 644 $f /etc/xs/lib.d/
+	sudo install -v -m 644 $f /etc/xs/lib.d/
 done
