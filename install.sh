efd4c2143cf2216186eaabb60dcf4c055a059262
David B. Lamkins <david@lamkins.net>
2020-12-24 18:13:13 -0800

    Do not reinstall unchanged files. Create a backup upon installing an updated file.

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@
 
 sudo install -v -m 755 -d /etc/xs/rc.d /etc/xs/lib.d
 for f in xs/rc.d/*; do
-	sudo install -v -m 644 $f /etc/xs/rc.d/
+	sudo install -v -b -C -m 644 $f /etc/xs/rc.d/
 done
 for f in xs/lib.d/*; do
-	sudo install -v -m 644 $f /etc/xs/lib.d/
+	sudo install -v -b -C -m 644 $f /etc/xs/lib.d/
 done
